5d9bb435dbba1fa66f58c78d9d01169c71021645
MiguelMascarenhas <miguel.v.mascarenhas@gmail.com>
2021-11-29 20:10:14 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/logalife/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.logalife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
